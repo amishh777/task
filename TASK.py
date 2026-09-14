8d1dd59936360c2b2cc0f32a8386b324ea3f7921
Task 1 Author <task1@localhost>
2026-09-14 19:07:21 +0530

    Add output score gradient

--- TASK.py
+++ TASK.py
@@ -130,3 +130,16 @@ for image_number in range(0, y_train.shape[0], 1):
 loss = total_loss / y_train.shape[0]
 #average loss over all training images
 print("Loss:", loss)
+
+d_scores = P.copy()
+
+for image_number in range(0, y_train.shape[0], 1):
+    correct_digit = y_train[image_number]
+    d_scores[image_number][correct_digit] = (
+        d_scores[image_number][correct_digit] - 1
+    )
+
+d_scores = d_scores / y_train.shape[0]
+
+print("d_scores shape:", d_scores.shape)
+print("First d_scores row:", d_scores[0])
